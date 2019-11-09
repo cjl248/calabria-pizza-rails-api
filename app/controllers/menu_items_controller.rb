d17700c66c55144e5151da335eefe1413b90aad4
github cjl248 <github cjl248@cornell.edu>
2019-11-08 23:26:32 -0500

    menu_items#show

--- app/controllers/menu_items_controller.rb
+++ app/controllers/menu_items_controller.rb
@@ -1,5 +1,14 @@
 class MenuItemsController < ApplicationController
 
+  def show
+    menu_item = MenuItem.find(params[:id])
+    render json: menu_item.to_json(
+      :include => {
+        :menu_category => {:except => [:created_at, :updated_at]}
+      },
+      :except => [:created_at, :updated_at])
+  end
+
   def index
     menu_items = MenuItem.all
     render json: menu_items.to_json(
